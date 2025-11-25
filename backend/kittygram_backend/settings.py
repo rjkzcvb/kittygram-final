0378fff0aa67c4eef0149e8b4744add1695fdc5d
Nikolay Smirnov <nikolaysmirnov8963@gmail.com>
2025-11-25 08:16:40 +0300

    Fix static files path: update STATIC_ROOT to match docker volume

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/app/collected_static"  # Директория в контейнере
+STATIC_ROOT = "/app/static"  # Директория в контейнере
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
